4601bf9c44d11f355b57d22153aec6394e83bdff
ha.ma <ha.matsumu@gmail.com>
2019-06-13 22:33:02 +0900

    create user and todo seed

---
+++ server/src/seeders/20190613132320-demo-todo.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const faker = require("faker");
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    const demoTodos = [];
+    for (let i = 1; i <= 50; i++) {
+      demoTodos.push({
+        title: faker.name.title(),
+        desc: faker.lorem.sentences(),
+        completed: faker.random.boolean(),
+        statusCode: Math.floor(Math.random() * 2),
+        orderNumber: i,
+        userId: Math.floor(Math.random() * 11)
+      });
+    }
+    console.log(demoTodos);
+    return queryInterface.bulkInsert("Todos", demoTodos, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Todos", null, {});
+  }
+};
