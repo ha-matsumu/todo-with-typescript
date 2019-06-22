16ac7d72e974c75d84843d6ccfc106addf23479f
ha.ma <ha.matsumu@gmail.com>
2019-06-22 14:10:52 +0900

    put '/' in one

--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -2,13 +2,13 @@ const express = require("express");
 const userController = require("../controllers/user.controller");
 const router = express.Router();
 
-// 新規登録
-router.post("/", userController.signup);
+// (POST:新規登録 DELETE:退会)
+router
+  .route("/")
+  .post(userController.signup)
+  .delete(userController.deactivate);
 
 // ログイン
 router.post("/login", userController.signin);
-
-// 退会
-router.delete("/", userController.deactivate);
 
 module.exports = router;
