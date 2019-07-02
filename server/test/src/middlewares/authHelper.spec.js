8b4b4bd415154a86099dd9ae8daffcd92a4102fe
ha.ma <ha.matsumu@gmail.com>
2019-07-02 22:34:03 +0900

    create test of jwt token

---
+++ server/test/src/middlewares/authHelper.spec.js
@@ -0,0 +1,68 @@
+const assert = require("power-assert");
+const faker = require("faker");
+const httpMocks = require("node-mocks-http");
+const { sequelize } = require("../../../src/models");
+const authHelper = require("../../../src/middlewares/authHelper");
+const requestHelper = require("../requestHelper");
+
+describe("Test of authHelper.js", () => {
+  const demoUser = {
+    name: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+
+  before(async () => {
+    await requestHelper
+      .requestAPI("post", "/users", 200)
+      .set("Accept", "application/json")
+      .send(demoUser);
+  });
+
+  after(async () => {
+    await sequelize.truncate();
+  });
+
+  it("トークン認証の動作確認 200", async () => {
+    const { body } = await requestHelper
+      .requestAPI("post", "/users/login", 200)
+      .set("Accept", "application/json")
+      .send(demoUser);
+
+    const token = body.token;
+    const req = httpMocks.createRequest({
+      headers: { "x-access-token": token }
+    });
+
+    const res = httpMocks.createResponse();
+    const verifiedToken = authHelper.verifyToken(req, res);
+
+    assert.equal(verifiedToken.statusCode, 200);
+  });
+
+  it("トークン認証の動作確認 401", async () => {
+    const req = httpMocks.createRequest({
+      headers: { "x-access-token": "token" }
+    });
+
+    const res = httpMocks.createResponse();
+    const verifiedToken = authHelper.verifyToken(req, res);
+    const payload = verifiedToken.output.payload;
+
+    assert.equal(payload.error, "Unauthorized");
+    assert.equal(payload.message, "Invalid token.");
+    assert.equal(payload.statusCode, 401);
+  });
+
+  it("トークン認証の動作確認 403", async () => {
+    const req = httpMocks.createRequest();
+
+    const res = httpMocks.createResponse();
+    const verifiedToken = authHelper.verifyToken(req, res);
+    const payload = verifiedToken.output.payload;
+
+    assert.equal(payload.error, "Forbidden");
+    assert.equal(payload.message, "No token provided.");
+    assert.equal(payload.statusCode, 403);
+  });
+});
--- server/test/src/routes/todo.router.spec.js
+++ server/test/src/routes/todo.router.spec.js
@@ -1,41 +1,110 @@
+const faker = require("faker");
+const { sequelize } = require("../../../src/models");
 const requestHelper = require("../requestHelper");
 
-describe("GET /todo/", () => {
-  it("リクエストの挙動確認 200", () => {
-    return requestHelper.requestAPI("get", "/todos", 200);
+describe("Test of todo.router.js", () => {
+  const demoUser = {
+    name: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+
+  before(async () => {
+    await requestHelper
+      .requestAPI("post", "/users", 200)
+      .set("Accept", "application/json")
+      .send(demoUser);
   });
 
-  it("リクエストの挙動確認 404", () => {
-    return requestHelper.requestAPI("get", "/todo", 404);
-  });
-});
-
-describe("POST /todo/", () => {
-  it("リクエストの挙動確認 200", () => {
-    return requestHelper.requestAPI("post", "/todos", 200);
+  after(async () => {
+    await sequelize.truncate();
   });
 
-  it("リクエストの挙動確認 404", () => {
-    return requestHelper.requestAPI("post", "/todo", 404);
-  });
-});
+  describe("GET /todos/", () => {
+    it("リクエストの挙動確認 200", async () => {
+      const { body } = await requestHelper
+        .requestAPI("post", "/users/login", 200)
+        .set("Accept", "application/json")
+        .send(demoUser);
 
-describe("PUT /todo/:id", () => {
-  it("リクエストの挙動確認 200", () => {
-    return requestHelper.requestAPI("put", "/todos/1", 200);
+      const token = body.token;
+      return requestHelper
+        .requestAPI("get", "/todos", 200)
+        .set("x-access-token", token);
+    });
+
+    it("リクエストの挙動確認 403", () => {
+      return requestHelper.requestAPI("get", "/todos", 403);
+    });
+
+    it("リクエストの挙動確認 404", () => {
+      return requestHelper.requestAPI("get", "/todo", 404);
+    });
   });
 
-  it("リクエストの挙動確認 404", () => {
-    return requestHelper.requestAPI("put", "/todo/1", 404);
-  });
-});
+  describe("POST /todos/", () => {
+    it("リクエストの挙動確認 200", async () => {
+      const { body } = await requestHelper
+        .requestAPI("post", "/users/login", 200)
+        .set("Accept", "application/json")
+        .send(demoUser);
 
-describe("DELETE /todo/", () => {
-  it("リクエストの挙動確認 200", () => {
-    return requestHelper.requestAPI("delete", "/todos/1", 200);
+      const token = body.token;
+      return requestHelper
+        .requestAPI("post", "/todos", 200)
+        .set("x-access-token", token);
+    });
+
+    it("リクエストの挙動確認 403", () => {
+      return requestHelper.requestAPI("post", "/todos", 403);
+    });
+
+    it("リクエストの挙動確認 404", () => {
+      return requestHelper.requestAPI("post", "/todo", 404);
+    });
   });
 
-  it("リクエストの挙動確認 404", () => {
-    return requestHelper.requestAPI("delete", "/todo/1", 404);
+  describe("PUT /todos/:id", () => {
+    it("リクエストの挙動確認 200", async () => {
+      const { body } = await requestHelper
+        .requestAPI("post", "/users/login", 200)
+        .set("Accept", "application/json")
+        .send(demoUser);
+
+      const token = body.token;
+      return requestHelper
+        .requestAPI("put", "/todos/1", 200)
+        .set("x-access-token", token);
+    });
+
+    it("リクエストの挙動確認 403", () => {
+      return requestHelper.requestAPI("put", "/todos/1", 403);
+    });
+
+    it("リクエストの挙動確認 404", () => {
+      return requestHelper.requestAPI("put", "/todo/1", 404);
+    });
+  });
+
+  describe("DELETE /todos/", () => {
+    it("リクエストの挙動確認 200", async () => {
+      const { body } = await requestHelper
+        .requestAPI("post", "/users/login", 200)
+        .set("Accept", "application/json")
+        .send(demoUser);
+
+      const token = body.token;
+      return requestHelper
+        .requestAPI("delete", "/todos/1", 200)
+        .set("x-access-token", token);
+    });
+
+    it("リクエストの挙動確認 403", () => {
+      return requestHelper.requestAPI("delete", "/todos/1", 403);
+    });
+
+    it("リクエストの挙動確認 404", () => {
+      return requestHelper.requestAPI("delete", "/todo/1", 404);
+    });
   });
 });
