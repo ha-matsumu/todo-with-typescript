332adfa1407d56446bd25fd9b0ff97fecd885e45
ha.ma <ha.matsumu@gmail.com>
2019-08-19 22:51:45 +0900

    change port to 8080

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const app = require("./server");
-const port = 3000;
+const port = 8080;
 
 app.listen(port, () => {
   console.log("Todo app listening on port", chalk.green(port), "!");
