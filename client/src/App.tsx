cbae986261645fe29d132d47619198f42f879deb
ha.ma <ha.matsumu@gmail.com>
2019-09-01 18:01:27 +0900

    add navbar components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import SideDrawer from './components/navbar/SideDrawer/SideDrawer';
+import Backdrop from './components/navbar/Backdrop/Backdrop';
 
 const App: React.FC = () => {
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
+  const clickDrawerToggleButtonHandler = () => {
+    setSideDrawerOpen(!sideDrawerOpen);
+  };
+
+  const clickBackdropHandler = () => {
+    setSideDrawerOpen(false);
+  };
+
+  let backdrop;
+  if (sideDrawerOpen) {
+    backdrop = <Backdrop clickBackdrop={clickBackdropHandler} />;
+  }
+
   return (
-    <div className="App">
-      <h1>App.tsx</h1>
+    <div className="App" style={{ height: '100%' }}>
+      <Navbar clickDrawerToggleButton={clickDrawerToggleButtonHandler} />
+      <SideDrawer hasShown={sideDrawerOpen} />
+      {backdrop}
+      <main style={{ marginTop: '64px' }}>
+        <p>This is the page content!</p>
+      </main>
     </div>
   );
 };
