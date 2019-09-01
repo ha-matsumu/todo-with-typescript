d5768dd1cfdcae8c2c475f03a50b2831adfca227
ha.ma <ha.matsumu@gmail.com>
2019-09-01 14:08:50 +0900

    create auth and todos components

---
+++ client/src/components/auth/SignUp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUp: React.FC = () => {
+  return <h1>SignUp Page</h1>;
+};
+
+export default SignUp;
---
+++ client/src/components/todos/TodoList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TodoList: React.FC = () => {
+  return <h1>TodoList Page</h1>;
+};
+
+export default TodoList;
